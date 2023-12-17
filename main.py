671c035a17aa86f7f3c13b304640f0394b49fec6
youyang <yyearth@163.com>
2023-12-17 16:22:38 +0800

    minor fix.

--- main.py
+++ main.py
@@ -11,33 +11,23 @@ import requests
 import re 
 from bs4 import BeautifulSoup
 import pandas as pd 
-import numpy as np
-import urllib3
 
-# os.environ['CURL_CA_BUNDLE'] = ''
+
 MAX_RETRY = 5
+INPUT_FILENAME = 'names'
+XLSX_FILENAME = 'data'
+MAX_GENINFO_LEN = 300
+WFO_URL = 'https://www.worldfloraonline.org/taxon/wfo-'
 
 requests.packages.urllib3.disable_warnings()
 
-urllib3.disable_warnings()
-
-scientific_name = []
-is_tree = []
-is_shrubs = []
-is_grass = []
-type = []
-detail_url = []
-general_info = []
-
-wfourl = 'https://www.worldfloraonline.org/taxon/wfo-'
-
-cookies = {
-    '_gid': 'GA1.2.915583917.1702651974',
-    'JSESSIONID': '4EEB3616EDD8C679A98CBB018EFB3DB3',
-    '_gat_gtag_UA_114340265_1': '1',
-    '_ga_4LYR7CZFD6': 'GS1.1.1702693795.2.1.1702698330.0.0.0',
-    '_ga': 'GA1.1.1154062738.1702651974',
-}
+# cookies = {
+#     '_gid': 'GA1.2.915583917.1702651974',
+#     'JSESSIONID': '4EEB3616EDD8C679A98CBB018EFB3DB3',
+#     '_gat_gtag_UA_114340265_1': '1',
+#     '_ga_4LYR7CZFD6': 'GS1.1.1702693795.2.1.1702698330.0.0.0',
+#     '_ga': 'GA1.1.1154062738.1702651974',
+# }
 
 def find_wfo_all_id(name='Pinus sylvestris') -> list[str]: 
 
@@ -110,7 +100,8 @@ def find_wfo_id(name='Pinus sylvestris') -> str:
     try: 
         tmp = find_wfo_all_id(name)[0]
     except IndexError as ie: 
-        print('\tempty.')
+        # print('\tempty.')
+        pass 
     return tmp 
 
 
@@ -137,7 +128,7 @@ def get_detail(id):
 
     while t < MAX_RETRY: 
         try: 
-            response = requests.get(wfourl + id, 
+            response = requests.get(WFO_URL + id, 
                                     # cookies=cookies, 
                                     headers=headers, 
                                     verify=False)
@@ -161,68 +152,65 @@ def get_detail(id):
 
 def get_by_name(name): 
 
-    wfoid = find_wfo_id(name)
-    if len(wfoid) == 0:
-        print('empty wfo id.')
+    wfo_id = find_wfo_id(name)
+    if len(wfo_id) == 0:
+        print('\tempty wfo id.')
         return (name, 'N/A', 'N/A', 'N/A', 'N/A', 'N/A', 'N/A')
     
-    detail = get_detail(wfoid)
-    
+    detail = get_detail(wfo_id)
+
     text = BeautifulSoup(detail, features='html.parser').get_text()
 
     text = text.replace('\t', ' ').lower()
 
-    tree_flag = 'N' 
-    shrubs_flag = 'N'
-    grass_flag = 'N'
+    is_tree = 'N' 
+    is_shrubs = 'N'
+    is_grass = 'N'
 
     # baum, árbol/Arboles, tree
     if text.find('tree') != -1 or text.find('baum') != -1 \
         or text.find('arboles') != -1 or text.find('árbol') != -1: 
         print('tree')
-        tree_flag = 'Y'
-        is_tree.append('Y')
+        is_tree = 'Y'
 
     #   sträucher, arbustos, shrubs
     if text.find('shrubs') != -1:
         print('shrubs')
-        shrubs_flag = 'Y'
-        is_shrubs.append('Y')
+        is_shrubs = 'Y'
 
     # Gras, hierba, grass 
     if text.find('grass') != -1 or text.find('gras') != -1 or text.find('hierba') != -1: 
         print('grass')
-        grass_flag = 'Y'
-        is_grass.append('Y')
+        is_grass = 'Y'
 
     info = ''
     idx = text.find('general information')
     if idx != -1:
-        info = text[idx + 19 : min(len(text), idx + 19 + 300)] + '...'
+        info = text[idx + 19 : (idx + 19 + MAX_GENINFO_LEN)] + '...'
 
-    general_info.append(info)
+    # general_info.append(info)
 
     typeinfo = ''
-    if is_tree: 
+    if is_tree == 'Y': 
         typeinfo += 'tree or '
     
-    if is_shrubs: 
+    if is_shrubs == 'Y': 
         typeinfo += 'shrubs or '
     
-    if is_grass: 
+    if is_grass == 'Y': 
         typeinfo += 'grass or '
 
     typeinfo = typeinfo.strip()
     if typeinfo.endswith('or'): 
         typeinfo = typeinfo[:-2]
 
-    return (name, tree_flag, shrubs_flag, grass_flag, typeinfo, wfourl+wfoid, info)
+    return (name, is_tree, is_shrubs, is_grass, typeinfo, WFO_URL+wfo_id, info)
 
 # scientific_name, is_tree, is_shrubs, is_grass, type, detail_url, general_info
 
 
 names = []
-with open('names.txt', 'r') as f:
+with open('{}.txt'.format(INPUT_FILENAME), 'r') as f:
     names = f.readlines()
 
 data = []
@@ -230,25 +218,25 @@ for name in names:
     name = name.strip()
     if len(name) == 0:
         continue
-    print('finding "{}"...'.format(name))
+    print('Finding "{}"...'.format(name))
     line = get_by_name(name)
-    print(line)
+    print(str(line)[:80])
     data.append(line)
 
-print(data)
+# print(data)
 
-data = np.array(data)
-
-df = pd.DataFrame(data, columns=["scientific_name", 
+df = pd.DataFrame(data, columns=[
+                                "scientific_name", 
                                 "is_tree", 
                                 "is_shrubs", 
                                 "is_grass", 
                                 "type", 
                                 "detail_url", 
-                                "general_info"])
+                                "general_info" ])
 
+print(df.head(3))
+print('...')
 
-
-df.to_excel("data.xlsx")
-
+df.to_excel("{}.xlsx".format(XLSX_FILENAME))
+print('{} records are saved to ./{}.xlsx'.format(df.shape[0], XLSX_FILENAME))
 
